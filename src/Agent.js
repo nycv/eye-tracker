357d0bb27cb8fb75eafbd593be140fe8994462d2
Sean Pollock <seanpollock3344@gmail.com>
2018-10-03 20:33:40 -0400

    wip agent moving in box. somewhat bouncing off of walls...

--- src/Agent.js
+++ src/Agent.js
@@ -1,42 +1,99 @@
+self.importScripts('../node_modules/gaussian/lib/gaussian.js')
 
 class Agent {
-  constructor(ctx) {
-    this.ctx = ctx
-    console.log('your name is neo')
+  constructor(gaussian) {
+    this.gaussian = gaussian
+    this.counter = 0
   }
-  
-  async gameLoop(frameInterval) {
-    await this.delay(frameInterval)
-    this.move()
-    this.draw()
-    return await this.gameLoop()
+
+  async gameLoop(interval) {
+    await this.delay(interval)
+    this.draw(this.x, this.y)
+    this.move(this.x, this.y)
+    this.counter++
+    return await this.gameLoop(interval)
+  }
+
+  init({ width, height }) {
+    this.width = width
+    this.height = height
+    this.initPosition()
+    this.gameLoop(100)
+  }
+
+  initPosition() {
+    // create initial target position
+    this.x = this.flatRandom(0, this.width)
+    this.y = this.flatRandom(0, this.height)
+    
+    // create initial target velocity
+    this.v = 3
+
+    // create initial direction
+    this.d = this.flatRandom(0, 360)
   }
   
   move(x, y) {
-    console.log('calculating move...')
+    this.checkWalls()
+    this.d = this.bellRandom(this.d, 5) % 360
+    this.calculatePosition()
+  }
+
+  calculatePosition() {
+    const yIncrement = Math.round(Math.sin(this.d * Math.PI / 180) * 5)
+    const xIncrement = Math.round(Math.sin((180 - this.d - 90) * Math.PI / 180 ) * 5)
+    this.x = this.x + xIncrement
+    this.y = this.y + yIncrement
   }
 
   draw(x, y) {
-    console.log('drawing circle on canvas')
+    self.postMessage({ x, y})   
+  }
+
+  checkWalls() {
+    if (this.x + 25 >= this.width) {
+      console.log('hit left wall')
+      this.d = this.d + 180
+    }
+     
+    if (this.x <= 0) { 
+      console.log('hit right wall')
+      this.d = this.d + 180
+    }
+         
+    if (this.y +25 >= this.height) {
+      this.d = this.d + 180
+      console.log('hit bottom wall')
+    }
+      
+    if (this.y <= 0) {
+      console.log('hit top wall')
+      this.d = this.d - 180
+    }
   }
 
   // flat random
-  
+  flatRandom(min, max) {
+    return Math.round(Math.random() * max) + min  
+  }
+
   // gaussian random
-  
+  bellRandom(mean, variance) {
+    const distribution = this.gaussian(mean, variance)
+    return Math.round(distribution.ppf(Math.random()))
+  } 
   
   // delay utlity function
   delay(time) {
     return new Promise((res, rej) => {
-      setTimeout(res(true), time)
+      setTimeout(() => res(true), time)
     })
   }
 }
 
-const main = () => {
-  console.log('trying to start script...')
-  const agent = new Agent()
-  agent.gameLoop()
-}
+console.log('web worker script started')
 
-main()
+const agent = new Agent(gaussian)
+
+self.onmessage = (msg) => agent.init(msg.data)
+
--- src/MousePad.js
+++ src/MousePad.js
@@ -2,16 +2,17 @@ import React, { Component } from 'react'
 
 export default class MousePad extends Component {
   componentDidMount = () => this.initCanvas()
-
+  
+  // create canvas element
   initCanvas = () => {
     this.ctx = this.refs.mouse_pad.getContext('2d')
     this.pad = this.refs.mouse_pad
     this.mediaStream = this.pad.captureStream(30)
     this.pad.addEventListener('mousemove', (evt) => {
       const mousePos = this.getMousePos(evt)
-      this.drawParticle(mousePos.x, mousePos.y)
     })
-
+    
+    // spawn web worker thread to handle game loop
     this.startAgentWorker()
   }
 
@@ -24,16 +25,34 @@ export default class MousePad extends Component {
   }
 
   drawParticle = (x, y) => {
+    if (this.x && this.y) this.cutParticle(this.x, this.y)
     this.ctx.beginPath()
-    this.ctx.fillStyle = 'green'
-    this.ctx.arc(x, y, 3, 0, Math.PI*2, true)
+    this.ctx.fillStyle = '#39ff14'
+    this.ctx.arc(x, y, 50, 0, Math.PI*2, true)
     this.ctx.closePath()
     this.ctx.fill()
+    this.x = x
+    this.y = y
+  }
+  
+  cutParticle = (x, y) => {
+    this.ctx.beginPath()
+    this.ctx.fillStyle=('#171717')
+    this.ctx.arc(x, y, 50, 0, Math.PI*2, true)
+    this.ctx.fill()
+    this.ctx.closePath()
   }
 
   startAgentWorker = () => {
     if (window.Worker) {
+      // spawn worker thread
       this.agent = new Worker('./src/Agent.js')
+
+      // send canvas dimension to worker
+      this.agent.postMessage({ width: this.pad.width, height: this.pad.height })
+
+      // receive position updates from worker
+      this.agent.onmessage = (msg) => this.drawParticle(msg.data.x, msg.data.y)
     }
   }
 
